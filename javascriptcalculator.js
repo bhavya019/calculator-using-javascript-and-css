bf0c06d7f45c1149af2071c7a19184c949b841ab
bhavya019 <66251706+bhavya019@users.noreply.github.com>
2022-06-27 23:09:08 +0530

    Add files via upload

---
+++ javascriptcalculator.js
@@ -0,0 +1,10 @@
+function display(data) {
+	document.getElementById('output').value +=data;
+}
+function erase(){
+	document.getElementById('output').value="";
+}
+function solve(){
+	var calc = document.getElementById('output').value;
+	document.getElementById('output').value = eval(calc);
+}
